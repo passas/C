2d869e8eb7f760874265f99c7bc8c4e57a72d280
Passinhas <48985218+passas@users.noreply.github.com>
2023-08-17 15:34:23 +0100

    [Queue-Array_] Queue.c

---
+++ Queue/Array (fixed size)/Queue.c
@@ -0,0 +1,111 @@
+#include <stdlib.h>
+// malloc
+// free
+
+#include "Queue.h"
+// typedef struct queue *Queue;
+
+struct queue {
+	int front;
+	int length;
+	int max;
+	void **queue;
+};
+
+int new_Queue (int size, Queue *q)
+{
+	int error;
+	Queue r;
+
+	error = 0;
+	r = (Queue) malloc (sizeof (struct queue));
+	if (r!=NULL)
+	{
+		r->queue = malloc (sizeof (void *) * size);
+		if (r->queue!=NULL)
+		{
+			r->front = 0;
+			r->length = 0;
+			r->max = size;
+		}
+		else
+		{
+			error = 1;
+			free (r);
+			(*q) = NULL;
+		}
+		(*q) = r;
+	}
+	else
+	{
+		error = 1;
+		(*q) = NULL;
+	}
+	return error;
+}
+
+void init_Queue (Queue *q)
+{
+	(*q)->front = (*q)->length = 0;
+}
+
+int isEmpty_Queue (Queue q)
+{
+	return !(q->length);
+}
+
+int enqueue_Queue (void *data, Queue *q)
+{
+	int error;
+	int pos;
+
+	error = 0;
+	if ((*q)->length < (*q)->max)
+	{
+		pos = ( (*q)->front + (*q)->length ) % (*q)->max;
+		(*q)->queue [ pos ] = data;
+		(*q)->length++;
+	}
+	else
+	{
+		error = 3;
+	}
+
+	return error;
+}
+
+int dequeue_Queue (Queue *q, void **x)
+{
+	int error;
+
+	error = 0;
+	if ( !isEmpty_Queue((*q)) ) //not empty
+	{
+		(*x) = (*q)->queue [ (*q)->front ];
+		(*q)->front = ( (*q)->front + 1 ) % (*q)->max;
+		(*q)->length--;
+	}
+	else
+	{
+		error = 2;
+	}
+
+	return error;
+}
+
+int front_Queue (Queue q, void **x)
+{
+	int error;
+
+	error = 0;
+	if ( !isEmpty_Queue((q)) ) //not empty
+	{
+		(*x) = (q)->queue [ (q)->front ];
+	}
+	else
+	{
+		error = 2;
+	}
+
+	return error;
+}
