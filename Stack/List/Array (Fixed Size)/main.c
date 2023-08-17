161cd4c6d8973d171b8dc8e17e224f16af7d00fe
Passinhas <48985218+passas@users.noreply.github.com>
2023-08-17 11:42:34 +0100

    [Stack-Array_fixed_size] Test

---
+++ Stack/List/Array (Fixed Size)/main.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+//printf
+
+#include <stdlib.h>
+//malloc(int)
+//free
+
+#include "Stack.h"
+// typedef struct stack *Stack;
+
+void emptyness (Stack *s);
+void pushing (Stack *s, int x);
+void popping (Stack *s);
+
+main ()
+{
+	Stack s;
+	new_Stack (3, &s);
+
+	init_Stack (&s);
+
+	emptyness (&s);
+
+	pushing (&s, 1);
+	pushing (&s, 2);
+	pushing (&s, 3);
+	pushing (&s, 4);
+
+	if (isEmpty_Stack (s))
+		printf ("Is Empty\n");
+	else
+		printf ("Not Empty\n");
+
+	popping (&s);
+	popping (&s);
+	popping (&s);
+	popping (&s); //4
+
+	emptyness(&s);
+}
+
+void emptyness (Stack *s)
+{
+	int *iptr;
+
+	if (pop_Stack (s, (void**)&iptr)==0)
+		printf ("%d\n", (*iptr));
+	else
+		printf ("No Pop\n");
+	
+	if (top_Stack (s, (void**)&iptr)==0)
+		printf ("%d\n", (*iptr));
+	else
+		printf ("No Top\n");
+	
+	if ( isEmpty_Stack ((*s)) )
+		printf ("Is Empty\n");
+	else
+		printf ("Not Empty\n");
+
+}
+void pushing (Stack *s, int x)
+{
+	int *i;
+	int *iptr;
+
+	i = (int *) malloc (sizeof (int));
+	(*i) = x;
+
+	push_Stack (i, s);
+
+	if (top_Stack (s, (void**)&iptr)==0)
+		printf ("%d\n", (*iptr));
+	else
+		printf ("No Top\n");
+}
+
+void popping (Stack *s)
+{
+	int *iptr;
+
+	if (pop_Stack (s, (void**)&iptr)==0)
+		printf ("%d\n", (*iptr));
+	else
+		printf ("No Pop\n");
+}
