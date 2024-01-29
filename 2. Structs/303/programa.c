1bf4a6cf3e1d00be2c5eae44abef81dae38c1745
Sérgio Costa <48985218+passas@users.noreply.github.com>
2024-01-29 22:19:07 +0000

    main

---
+++ 2. Structs/303/programa.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Headers relativos à entidade pessoa
+#include "pessoa.h"
+
+
+main ()
+{
+	//Apontador para a struct pessoa
+	Pessoa p;
+	//Variável de suporte à construção da string relativa ao conteúdo da Pessoa
+	char *str;
+
+	//pessoas.h: Criar estrutura pessoa -p- e preencher os campos
+	p = criar_Pessoa (1, "Miguel");
+
+	//pessoas.h: Criar string com o conteúdo da estrutura pessoa -p-
+	str = str_Pessoa (p);
+	//-stdout-: Conteúdo da estrutura pessoa -p-
+	printf ("%s\n", str);
+	//Libertar string com o conteúdo da estrutura pessoa -p-
+	free (str);
+
+	//pessoas.h: Libertar memória -heap- reservada para a estrutura pessoa -p-
+	free_Pessoa (p);
+}
